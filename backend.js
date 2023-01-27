7d52df4e69c86b943969f90e127d7129d6c6602d
Mangesh1511 <manpatil1451@gmail.com>
2023-01-27 20:55:20 +0530

    City map added with the weather forecast

--- backend.js
+++ backend.js
@@ -26,14 +26,16 @@ app.post('/', function (req, res) {
     axios.get(url)
         .then((response) => {
             const obj = response.data;
-           
+
 
             var data1 = JSON.parse(JSON.stringify(obj));
             console.log(data1.weather[0].main);
-            descr="Feels like: "+data1.weather[0].description;
-            var hum="Humidity: "+obj.main.humidity+"% "
-            var temp_min_max = "Min: " + obj.main.temp_min + " | Max: " + obj.main.temp_max;
-            res.render('index', { weather: obj, sit:descr, humidity: hum, city: obj.name, country: obj.sys.country, icon: obj.weather.icon, temp:"Temp:"+obj.main.temp, tempminmax: temp_min_max, errmsg:null });
+            descr = "Feels like: " + data1.weather[0].description;
+            var hum = "Humidity: " + obj.main.humidity + "% "
+            // var temp_min_max = "Min: " + obj.main.temp_min + " | Max: " + obj.main.temp_max;
+            var temp_min = "Min: " + obj.main.temp_min;
+            var temp_max = "Max: " + obj.main.temp_max;
+            res.render('index', { weather: obj, sit: descr, humidity: hum, city: obj.name, country: obj.sys.country, icon: obj.weather.icon, temp: "Temp:" + obj.main.temp, lon: obj.coord.lon, lat: obj.coord.lat, tempmin: temp_min, tempmax: temp_max, errmsg: null });
         })
         .catch((err) => {
             console.log(err);
@@ -43,6 +45,6 @@ app.post('/', function (req, res) {
 })
 
 app.listen(5000, () => {
-    
+
     console.log("Server is Listening at Port 5000");
 })
